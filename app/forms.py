dd8f1c4e504000a776a2aa044eb0b68c3fd3ec1d
sgmqs <674663550@qq.com>
2017-12-22 21:55:59 +0800

    'ts'

--- app/forms.py
+++ app/forms.py
@@ -6,10 +6,34 @@
 # @Version : $Id$
 
 from flask.ext.wtf import Form
-
-from wtforms import StringField,BooleanField
-from wtforms.validators import DataRequired
+from wtforms import StringField,BooleanField,TextAreaField,IntegerField
+from wtforms.validators import DataRequired,Length
+from app.models import User
 class LoginForm(Form):
 	"""docstring for LoginForm"""
 	openid	=	StringField('openid',validators=[DataRequired()])
 	remember_me	=	BooleanField('remember_me',default=False)
+
+
+class EditForm(Form):
+	"""docstring for EditForm"""
+	nickname	=	StringField('nickname',validators=[DataRequired()])
+	about_me	=	TextAreaField('about_me',validators=[Length(min=0,max=140)])
+	uid 		=	IntegerField('uid', validators=[DataRequired()])
+
+	def __init__(self,original_nickname,*arg,**kwargs):
+		Form.__init__(self,*arg,**kwargs)
+		self.original_nickname	=	original_nickname
+
+	def validate(self):
+		if not Form.validate(self):
+			return False
+		if self.nickname.data==self.original_nickname:
+			return True
+		user = User.query.filter_by(nickname=self.nickname.data).first()
+		if user !=None:
+			self.nickname.errors.append('This nickname:{0} is already in database'.format(self.nickname.data))
+			return False
+		return True
+
+		
--- app/views.py
+++ app/views.py
@@ -7,7 +7,7 @@
 from app import app,db,lm,oid
 from flask import render_template,flash,redirect,session,url_for,request,g
 from flask.ext.login import login_user,logout_user,current_user,login_required
-from .forms import LoginForm
+from .forms import LoginForm,EditForm
 from .models import User
 from datetime import datetime
 
@@ -54,9 +54,18 @@ def login():
 		user =	User.query.filter_by(nickname=form.openid.data).first()
 		if user is not None:
 			session['nickname']=user.nickname 
+			session['id']	=	user.id
 			return redirect(url_for('index'))
 		else:
-			return oid.try_login(form.openid.data,ask_for=['nickname','email'])
+			# return oid.try_login(form.openid.data,ask_for=['nickname','email'])
+			nickname =	User.make_unique_nickname(form.openid.data)
+			user = User(nickname=nickname,email=nickname+'@sgmqsblog.com')
+			db.session.add(user)
+			db.session.commit()
+			print("---ppp-------------",user.id)
+			session['nickname'] = nickname
+			session['id']	=	user.id
+			return redirect(url_for('index'))
 
 	return render_template('login.html',
 		title='Sign In',
@@ -67,8 +76,10 @@ def login():
 
 @app.before_request
 def before_request():
+	pass
 	 # g.user = current_user
-	if session['nickname'] is not None:
+	#print(dir(current_user.nickname))
+	# if session['nickname'] is not None:
 		# User.last_seen	=	datetime.utcnow()
 		# User.nickname 	=	session['nickname']
 		## sqlalchery session orm 配合commit使用   类库 session 做事务处理
@@ -76,7 +87,7 @@ def before_request():
 		#db.session.commit()
 		# User.update().where(User.nickname==session['nickname']).values(last_seen=datetime.utcnow())
 		## sqlalchery orm   类库
-		User.query.filter(User.nickname==session['nickname']).update({'last_seen':datetime.utcnow()})
+		# User.query.filter(User.nickname==session['nickname']).update({'last_seen':datetime.utcnow()})
 		# db.session.add(User)
 		
 
@@ -85,26 +96,38 @@ def before_request():
 def load_user(id):
 	return User.query.get(int(id))
 
-@oid.after_login
-def after_login(resp):
+# @oid.after_login
+# def after_login(resp):
 
-	if resp.email is None or resp.email =="":
-		flash('Invalid login,Please try again')
-		return redirect(url_for('login'))
-	user =	User.query.filter_by(email=resp.email).first()
-	if user is None:
-		nickname = resp.nickname
-		if nickname is None or nickname=="":
-			nickname = resp.email.split('@')[0]
-		user = User(nickname=nickname,email=resp.email)
-		db.session.add(user)
-		db.session.commit()
-	remember_me = False
-	if 'remember_me' in session:
-		remember_me = session['remember_me']	
-		session.pop('remember_me',None)
-	login_user(user,remember=remember_me)
-	return redirect(request.args.get('next') or url_for('index'))
+# 	if resp.email is None or resp.email =="":
+# 		flash('Invalid login,Please try again')
+# 		return redirect(url_for('login'))
+# 	user =	User.query.filter_by(email=resp.email).first()
+# 	if user is None:
+# 		nickname = resp.nickname
+# 		if nickname is None or nickname=="":
+# 			nickname = resp.email.split('@')[0]
+# 		user = User(nickname=nickname,email=resp.email)
+# 		db.session.add(user)
+# 		db.session.commit()
+# 	remember_me = False
+# 	if 'remember_me' in session:
+# 		remember_me = session['remember_me']	
+# 		session.pop('remember_me',None)
+
+
+# 	nickname =	User.make_unique_nickname('ppppp')
+
+# 	print("---user---dddd4444----------",nickname)
+
+# 	user = User(nickname=nickname)
+
+# 	db.session.add(user)
+# 	intid = db.session.commit()
+# 	print("---ppp-------------",intid)
+# 	session['nickname'] = nickname
+# 	login_user(user,remember=remember_me)
+# 	return redirect(request.args.get('next') or url_for('index'))
 
 
 @app.route('/logout')
@@ -128,3 +151,35 @@ def user(nickname):
 		 posts = posts,
 		 user = user
 		)
+
+@app.route('/edit',methods=['GET','POST'])
+def edit():
+
+	editform 	=	EditForm(session['nickname'])
+	if editform.validate_on_submit():
+		session['nickname']	=	editform.nickname.data
+		uid =request.form.get("user_id")
+		print("dd:",editform.about_me.data)
+		#User.query.filter(User.id==uid).update({'about_me':'ddd'})
+		# User.query.filter(User.id==uid).update({'about_me':datetime.utcnow()})
+		# User.query.filter(User.nickname==session['nickname']).update({'about_me':datetime.utcnow()})
+		#更新，添加 删除都要commit
+		db.session.query(User).filter(User.id==uid).update({'nickname':editform.nickname.data,'about_me':editform.about_me.data})
+		db.session.commit()
+		flash('Your changes have been saved.')
+		return redirect(url_for('edit'))
+	else:
+		user = User.query.filter_by(nickname=session['nickname']).first()
+		editform.nickname.data = user.nickname
+		editform.about_me.data = user.about_me
+		return render_template('edit.html',form=editform)
+
+@app.errorhandler(404)
+def internal_error(error):
+	return render_template('404.html'),404
+
+@app.errorhandler(500)
+def internal_error(error):
+	db.session.rollback()
+	return render_template('500.html'),500
+
